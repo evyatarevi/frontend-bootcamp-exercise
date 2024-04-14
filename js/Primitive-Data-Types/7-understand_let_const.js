3a66cf069ffec5dcb6c109ec9531c71470fba4e9
evyatarevi <117386537+evyatarevi@users.noreply.github.com>
2024-04-14 14:07:28 +0300

    added let&const ex

---
+++ js/Primitive-Data-Types/7-understand_let_const.js
@@ -0,0 +1,32 @@
+// 1
+var num = 2;
+
+// 2
+let myName = 'Evyatar';
+
+// 3
+const bool = true;
+
+// 4
+var number = 1;
+console.log(number);
+number = 2;
+console.log(number);
+
+// 5
+let str = 'evyatar';
+console.log(str);
+str = 'ifrach';
+console.log(str);
+
+// 6
+const num2 = 5;
+console.log(num2);
+num2 = 5;
+console.log(num2);
+
+/*
+why it is recommend to use always in const?
+not always, but when you won't to change th value.
+const helps prevent bugs and makes the code easier to understand. // in this line I was helped in AI.
+*/
