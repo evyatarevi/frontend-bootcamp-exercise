c44b549b3545b87cf34fa0e484386a9c206109dc
evyatarevi <117386537+evyatarevi@users.noreply.github.com>
2024-04-14 12:54:37 +0300

    added ex 3

---
+++ js/Primitive-Data-Types/3-primitive-data.js
@@ -0,0 +1,17 @@
+// 1
+const myName = 'Evyatar';
+
+// 2
+const num1 = 2;
+const num2 = 3;
+console.log(num1+num2);
+
+// 3
+const bool = true;
+
+// 4
+let unValue;
+console.log(unValue);
+
+// 5
+const nully = null;
