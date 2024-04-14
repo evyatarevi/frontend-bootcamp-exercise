598ed88e09986859b3b2bfebd959dc648fcb1edc
evyatarevi <117386537+evyatarevi@users.noreply.github.com>
2024-04-14 13:13:37 +0300

    added string-conc ex

---
+++ js/Primitive-Data-Types/5-string_concatenation.js
@@ -0,0 +1,19 @@
+// 1
+const myName = 'Evyatar';
+const myAge = 30;
+console.log(`My name is ${myName} and I am ${myAge} years old.`);
+
+// 2
+const favColor = 'Whit';
+const favMovie = 'The King Lion';
+const favFood = 'Pizza';
+console.log(`My favorite color is ${favColor}, I love the movie ${favMovie}, and I enjoy eating ${favFood}`);
+
+// 3
+const a = 1;
+const b = 2;
+console.log(`The sum of ${a} and ${b} is ${a+b}`);
+
+// 4
+const today = "Munday";
+console.log('Today is ' + today);
