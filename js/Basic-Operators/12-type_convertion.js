f0edf330f72dbe5d7f5ab8b9605801dcf0890e89
evyatarevi <117386537+evyatarevi@users.noreply.github.com>
2024-04-14 22:12:56 +0300

    12-type_conversion

---
+++ js/Basic-Operators/12-type_convertion.js
@@ -0,0 +1,32 @@
+// 1
+const x = 123;
+console.log(x.toString());
+console.log(String(x));
+
+// 2
+console.log(Number('456'));
+console.log(+'456');
+
+// 3
+console.log(parseFloat('78.9'));
+
+// 4
+console.log('3' + 5); //the result is number.
+
+// 5
+console.log(Boolean(1));
+console.log(Boolean(0));
+
+// 6
+console.log(+'24');
+
+// 7
+console.log(Number(true));
+console.log(Number(false));
+
+// 8
+console.log(Number(null));
+
+
+
+
