be70409bac4154560f909bf1fe3f0fb1f9faaccf
evyatarevi <117386537+evyatarevi@users.noreply.github.com>
2024-04-14 15:42:25 +0300

    10-order=operation

---
+++ js/Basic-Operators/10-order_operations.js
@@ -0,0 +1,18 @@
+/*
+
+1. 19
+2. 9
+3. 18
+4. 20
+5. 16
+6. 14 and 20
+7. 12.5
+8. 8
+9. 14
+10. '3030' and '301020'
+11. 13
+12. error, impossible to change constant. If it was let variable: x++*2 = 20 and x = 11.
+13. Infinity
+14. 3
+
+*/
